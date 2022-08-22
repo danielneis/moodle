14064df249697264de7debe84add4aeb41495c82
Daniel Neis Araujo <daniel@adapta.online>
2022-08-22 10:20:07 -0300

    corrige acesso de p_administrador aos relatórios

--- admin/settings/plugins.php
+++ admin/settings/plugins.php
@@ -699,8 +699,10 @@ if ($hassiteconfig) {
 
 // Content bank content types.
 
-if ($hassiteconfig ||
-    user_has_role_assignment($USER->id, $DB->get_field('role', 'id', ['shortname' => 'p_administrador']), $systemcontext->id)) {
+global $USER;
+$canconfigcontentbank = $hassiteconfig ||
+    user_has_role_assignment($USER->id, $DB->get_field('role', 'id', ['shortname' => 'p_administrador']), $systemcontext->id);
+if ($canconfigcontentbank) {
     $ADMIN->add('modules', new admin_category('contentbanksettings', new lang_string('contentbank')));
     $temp = new admin_settingpage('managecontentbanktypes', new lang_string('managecontentbanktypes'));
     $temp->add(new admin_setting_managecontentbankcontenttypes());
@@ -714,7 +716,7 @@ if ($hassiteconfig ||
     $plugins = core_plugin_manager::instance()->get_plugins_of_type('contenttype');
     foreach ($plugins as $plugin) {
         /** @var \core\plugininfo\contentbank $plugin */
-        $plugin->load_settings($ADMIN, 'contentbanksettings', $hassiteconfig);
+        $plugin->load_settings($ADMIN, 'contentbanksettings', $canconfigcontentbank);
     }
 }
 

5e345ef9842c55057cb2ef48d5923a36985b9067
daniel <daniel@adapta.online>
2023-09-18 10:33:39 -0300

    SPR: alterações auth db

--- auth/db/classes/task/sync_spr.php
+++ auth/db/classes/task/sync_spr.php
@@ -14,35 +14,39 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Task definition for auth_db.
- * @author    Guy Thomas <gthomas@moodlerooms.com>
- * @copyright Copyright (c) 2017 Blackboard Inc.
- * @package   auth_db
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+namespace auth_db\task;
 
 defined('MOODLE_INTERNAL') || die();
 
-$tasks = array(
-    array(
-        'classname' => '\auth_db\task\sync_users',
-        'blocking' => 0,
-        'minute' => 'R',
-        'hour' => 'R',
-        'day' => '*',
-        'month' => '*',
-        'dayofweek' => '*',
-        'disabled' => 1
-    ),
-    array(
-        'classname' => '\auth_db\task\sync_spr',
-        'blocking' => 0,
-        'minute' => 'R',
-        'hour' => 'R',
-        'day' => '*',
-        'month' => '*',
-        'dayofweek' => '*',
-        'disabled' => 1
-    )
-);
+/**
+ * Sync SPR task class
+ * @package   auth_db
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class sync_spr extends \core\task\scheduled_task {
+
+    /**
+     * Name for this task.
+     *
+     * @return string
+     */
+    public function get_name() {
+        return get_string('auth_dbsyncsprtask', 'auth_db');
+    }
+
+    /**
+     * Run task for synchronising users.
+     */
+    public function execute() {
+        if (!is_enabled_auth('db')) {
+            mtrace('auth_db plugin is disabled, synchronisation stopped', 2);
+            return;
+        }
+
+        $dbauth = get_auth_plugin('db');
+        $config = get_config('auth_db');
+        $trace = new \text_progress_trace();
+        $update = !empty($config->updateusers);
+        $dbauth->sync_spr($trace, $update);
+    }
+}
